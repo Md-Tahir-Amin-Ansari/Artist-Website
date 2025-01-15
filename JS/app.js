64f68a38f8a789a67ae31479e3d5cec87e8f3ed8
Md-Tahir-Amin-Ansari <mdtahir.amin.ansari@gmail.com>
2025-01-15 19:22:05 +0530

    added favicon . added auto close for menu. fixed a bug where AOS was causing overflow issue.

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+document.querySelectorAll(".menuItem").forEach(item => {
+    item.addEventListener('click', closeMenu);
+});
+
+function closeMenu(){
+    const menu = document.getElementById('menu');
+    const navbarToggle = document.getElementById('navbarToggle');
+    navbarToggle.classList.add("collapsed");
+    menu.classList.remove("show");
+}
